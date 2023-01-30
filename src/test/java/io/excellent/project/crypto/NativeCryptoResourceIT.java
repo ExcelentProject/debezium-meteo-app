1f530959ccdecee0a25ff9c206dc1c4bd6e9fe6e
Ondrej Babec <ond.babec@gmail.com>
2023-01-30 14:05:04 +0100

    Initial structure

---
+++ src/test/java/io/excellent/project/crypto/NativeCryptoResourceIT.java
@@ -0,0 +1,9 @@
+package io.excellent.project.crypto;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCryptoResourceIT extends CryptoResourceTest {
+
+    // Execute the same tests but in native mode.
+}
